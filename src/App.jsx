9d2d1f8a7061ce76f13f44a0430a2fdf369832ea
khanRabiul <krabiulislam2@gmail.com>
2024-12-09 12:34:33 +0600

    tailwind-react-vite-pnpm template

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return <>
+  <div className="h-screen flex items-center justify-center w-full bg-pink-600 text-5xl">
+    <h1>Welcome to Tailwindcss, react.js, vite and pnpm Template</h1>
+  </div>
+  </>;
+}
+
+export default App;
